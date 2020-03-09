198b639dc8dd0b3bc34b65fd4a6aeb69a1a8b479
MadhukarGosula <60699770+MadhukarGosula@users.noreply.github.com>
2020-03-09 15:23:42 +0530

    Update SingletonPatternExample.java

--- SingletonPatternExample.java
+++ SingletonPatternExample.java
@@ -1,1 +1,23 @@
 
+class Singleton{
+private static Singleton firstInstance=null;
+private Singleton(){
+}
+public static Singleton getInstance()
+{
+if(firstInstance==null)
+{
+firstInstance=new Singleton();
+}
+return firstInstance;
+}
+}
+public class SingletonPatternExample{
+public static void main(String[] args)
+{
+Singleton instance=Singleton.getInstance();
+System.out.println(instance);
+Singleton Instance=Singleton.getInstance();
+System.out.println(instance);
+}
+}
